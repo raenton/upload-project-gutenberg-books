c3525b55369d7e69837c50ab72ac77f9f5171e8f
Robin Denton <raenton@gmail.com>
2020-06-26 21:01:00 +0100

    add semis to env

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,17 @@
-const path = require('path')
+const path = require('path');
 
-let envFile
+let envFile;
 
 if (process.env.NODE_ENV === 'development') {
-  envFile = path.resolve(process.cwd(), '.env.development')
+  envFile = path.resolve(process.cwd(), '.env.development');
 } else if (process.env.NODE_ENV === 'test') {
-  envFile = path.resolve(process.cwd(), '.env.test')
+  envFile = path.resolve(process.cwd(), '.env.test');
 } else {
-  envFile = path.resolve(process.cwd(), '.env')
+  envFile = path.resolve(process.cwd(), '.env');
 }
 
 require('dotenv').config({
   path: envFile
-})
+});
 
-module.exports = process.env
+module.exports = process.env;
